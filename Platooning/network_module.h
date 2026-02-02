bcb9c81cfec7311419ef5e661f24a3eb9e0cb8dd
Neaz Mahmud <72194480+Neaz145126@users.noreply.github.com>
2026-02-03 00:10:45 +0100

    Platoon size increased 10 from 5

--- Platooning/network_module.h
+++ Platooning/network_module.h
@@ -77,7 +77,7 @@ public:
     }
 
     void broadcast(const PlatoonMessage& msg) {
-        for(int i=0; i<5; i++) {
+        for(int i=0; i<MAX_NODES; i++) {
             broadcastAddr.sin_port = htons(PORT_BASE + i);
             if (i != msg.truckId) {
                 sendto(sockfd, (const char*)&msg, sizeof(msg), 0, (sockaddr*)&broadcastAddr, sizeof(broadcastAddr));
